7949c14d0cacc432bdc7e8208517e2601f785b12
Lovizu <nug22kr@gmail.com>
2018-05-24 23:28:21 +0900

    # 구글광고 주석

--- resources/views/seo/ad.blade.php
+++ resources/views/seo/ad.blade.php
@@ -1,10 +1,10 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Lovizu -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1322828698634218"
-     data-ad-slot="4202942508"
-     data-ad-format="auto"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+{{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+{{--<!-- Lovizu -->--}}
+{{--<ins class="adsbygoogle"--}}
+     {{--style="display:block"--}}
+     {{--data-ad-client="ca-pub-1322828698634218"--}}
+     {{--data-ad-slot="4202942508"--}}
+     {{--data-ad-format="auto"></ins>--}}
+{{--<script>--}}
+    {{--(adsbygoogle = window.adsbygoogle || []).push({});--}}
+{{--</script>--}}
