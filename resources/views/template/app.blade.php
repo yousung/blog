fc57a1ab465be11ea7875a7fed6159a9e8fcc1e1
yousung <nug22kr@gmail.com>
2018-05-24 17:22:25 +0900

    # 다국어 SEO 적용

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 
+	<link rel="alternate" hreflang="ko" href="{{ \Request::fullUrl() }}">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css">
@@ -13,6 +14,8 @@
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<link href='{{ mix('/css/clean-blog.css') }}' rel='stylesheet' type='text/css'>
 	<link rel="shortcut icon" href="/favicon.ico">
+
+
 
 	{!! SEO::generate() !!}
 	@stack('style')
