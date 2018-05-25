3debcaaffc1c0c4ff699625b90fcb816e7598fb1
Lovizu <nug22kr@gmail.com>
2018-05-26 01:40:48 +0900

    # google adsense 다시 적용

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -16,16 +16,6 @@
 	<script>
 		console.log('This post is {{ $post->hit }} hit');
 	</script>
-
-	<!-- 포스팅 -->
-	<ins class="adsbygoogle"
-		 style="display:block"
-		 data-ad-client="ca-pub-1322828698634218"
-		 data-ad-slot="6504831897"
-		 data-ad-format="auto"></ins>
-	<script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
 @endsection
 
 @section('content')
