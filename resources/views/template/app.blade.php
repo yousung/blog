407f5abba7ed3e5c44846660120197bef88920fc
yousung <nug22kr@gmail.com>
2018-05-25 12:26:18 +0900

    # 원복

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -15,12 +15,12 @@
 
 	@include('seo.google')
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css">
-	<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-	<link href='{{ mix('/css/clean-blog.css') }}' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css">
+	<link rel="stylesheet" type="text/css" href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic'>
+	<link rel="stylesheet" type="text/css" href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'>
+	<link rel="stylesheet" type="text/css" href='{{ mix('/css/clean-blog.css') }}'>
 </head>
 <body>
 <!-- Navigation -->
