0c75e28c1dd115abe4638304c5e8d1de03761e6b
yousung <nug22kr@gmail.com>
2018-05-25 18:35:39 +0900

    # 메일작성 에러 처리

--- resources/views/seo/google.blade.php
+++ resources/views/seo/google.blade.php
@@ -11,10 +11,10 @@
 
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-1322828698634218",
-        enable_page_level_ads: true
-    });
+    // (adsbygoogle = window.adsbygoogle || []).push({
+    //     google_ad_client: "ca-pub-1322828698634218",
+    //     enable_page_level_ads: true
+    // });
 </script>
 
 {!! isset($itemSchma) ? $itemSchma->toScript() : '' !!}
