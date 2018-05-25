d5a060bd7d1aad5ebb80916195574ebf776fdd5f
Lovizu <nug22kr@gmail.com>
2018-05-26 01:33:28 +0900

    # google adsense 다시 적용

--- resources/views/seo/google.blade.php
+++ resources/views/seo/google.blade.php
@@ -9,13 +9,5 @@
     gtag('config', 'UA-112233205-2');
 </script>
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-1322828698634218",
-        enable_page_level_ads: true
-    });
-</script>
-
 {!! isset($itemSchma) ? $itemSchma->toScript() : '' !!}
 {!! isset($itemListSchma) ? $itemListSchma->toScript() : '' !!}
