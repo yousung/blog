4bae3895e65efe72e2ff89eb66c82ba66c391f4c
yousung <nug22kr@gmail.com>
2018-05-25 12:16:17 +0900

    # 원복

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -7,6 +7,12 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 
 	<link rel="alternate" hreflang="ko" href="{{ \Request::fullUrl() }}">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css">
+	<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+	<link href='{{ mix('/css/clean-blog.css') }}' rel='stylesheet' type='text/css'>
 	<link rel="shortcut icon" href="/favicon.ico">
 
 	{!! SEO::generate() !!}
@@ -25,9 +31,6 @@
 <hr>
 
 <!-- Footer -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css">
 @include('template.footer')
 @stack('script')
 @yield('script')
--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -28,9 +28,6 @@
 </footer>
 
 <!-- After Css loader -->
-<link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-<link href='{{ mix('/css/clean-blog.css') }}' rel='stylesheet' type='text/css'>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css" rel="stylesheet" type="text/css" />
 <link href="//cdn.jsdelivr.net/highlight.js/8.7/styles/monokai_sublime.min.css" rel="stylesheet" type="text/css">
 
