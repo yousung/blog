24b458fd622480b40dacd6fe250484ba3ec30c91
Lovizu <nug22kr@gmail.com>
2018-05-26 12:09:24 +0900

    # 광고 하단 자동으로 변경

--- resources/views/seo/bottom.blade.php
+++ resources/views/seo/bottom.blade.php
@@ -1,12 +1,22 @@
+{{--<br/>--}}
+{{--<!-- 하단 -->--}}
+{{--<ins class="adsbygoogle"--}}
+     {{--style="display:block"--}}
+     {{--data-ad-client="ca-pub-1322828698634218"--}}
+     {{--data-ad-slot="8870021224"--}}
+     {{--data-ad-format="auto"></ins>--}}
+{{--<script>--}}
+    {{--(adsbygoogle = window.adsbygoogle || []).push({});--}}
+{{--</script>--}}
+{{--<br/>--}}
+{{--<hr/>--}}
+
 <br/>
-<!-- 하단 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1322828698634218"
-     data-ad-slot="8870021224"
-     data-ad-format="auto"></ins>
 <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-1322828698634218",
+        enable_page_level_ads: true
+    });
 </script>
 <br/>
 <hr/>
