9d9931561546fd682dc6f7862e06b2229b7f007e
Lovizu <nug22kr@gmail.com>
2018-05-26 12:11:40 +0900

    # 상단 광고 변경

--- resources/views/seo/bottom.blade.php
+++ resources/views/seo/bottom.blade.php
@@ -1,22 +1,12 @@
-{{--<br/>--}}
-{{--<!-- 하단 -->--}}
-{{--<ins class="adsbygoogle"--}}
-     {{--style="display:block"--}}
-     {{--data-ad-client="ca-pub-1322828698634218"--}}
-     {{--data-ad-slot="8870021224"--}}
-     {{--data-ad-format="auto"></ins>--}}
-{{--<script>--}}
-    {{--(adsbygoogle = window.adsbygoogle || []).push({});--}}
-{{--</script>--}}
-{{--<br/>--}}
-{{--<hr/>--}}
-
 <br/>
+<!-- 하단 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1322828698634218"
+     data-ad-slot="8870021224"
+     data-ad-format="auto"></ins>
 <script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-1322828698634218",
-        enable_page_level_ads: true
-    });
+    (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 <br/>
 <hr/>
--- resources/views/seo/top.blade.php
+++ resources/views/seo/top.blade.php
@@ -1,10 +1,7 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 상단 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1322828698634218"
-     data-ad-slot="6504831897"
-     data-ad-format="auto"></ins>
 <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-1322828698634218",
+        enable_page_level_ads: true
+    });
 </script>
