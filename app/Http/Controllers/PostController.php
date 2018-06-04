46e0f70aabed09ebbbc07af91ba76213a62cda30
yousung <nug22kr@gmail.com>
2018-06-04 15:46:58 +0900

    # seo 로직 변경

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,16 +21,21 @@ class PostController extends Controller implements Cacheble
     public function index(PostFilter $filter)
     {
         if ($search = \Request::input('search')) {
-            $this->seo("'{$search}' 검색결과", "'{$search}' 로 포스트 제목, 부제목, 태그, 시리즈를 검색합니다");
+            $title = "'{$search}' 검색결과";
+            $desc = "'{$search}' 로 포스트 제목, 부제목, 태그, 시리즈를 검색합니다";
         } elseif ($tag = \Request::input('tag')) {
-            $this->seo("'{$tag}' 검색결과", "'{$tag}' 로 태그 검색합니다");
+            $title = "'{$tag}' 검색결과";
+            $desc = "'{$tag}' 로 태그 검색합니다";
         } elseif ($series = \Request::input('series')) {
-            $this->seo("'{$series}' 검색결과", "'{$series}' 리스트 입니다");
+            $title ="'{$series}' 검색결과";
+            $desc = "'{$series}' 리스트 입니다";
         } else {
-            $this->seo('포스트 전체보기', '작성된 모든 포스트를 확인할 수 있습니다.');
+            $title ='포스트 전체보기';
+            $desc = '작성된 모든 포스트를 확인할 수 있습니다.';
         }
 
         $query = Post::Filter($filter);
+        $this->seo($title, $desc);
 
         $posts = $this->cache(cache_key(), $query, 'simplePaginate', 15);
         $this->postList($posts);
