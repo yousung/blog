7a62659edaf64e90583836049258578729cc0819
yousung <nug22kr@gmail.com>
2018-06-05 14:03:19 +0900

    # naver-xmlrpc composer 로 변경

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-29 16:32:32.
+    * Generated on 2018-06-05 13:42:24.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- app/Listeners/NaverBlogListener.php
+++ app/Listeners/NaverBlogListener.php
@@ -2,29 +2,19 @@
 
 namespace App\Listeners;
 
-use App\Events\ModelChange;
-use PhpXmlRpc\Value;
-use PhpXmlRpc\Client;
-use PhpXmlRpc\Request;
 use App\Events\NaverBlog;
+use App\Events\ModelChange;
+use Lovizu\NaverXmlRpc\NaverBlogXml;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class NaverBlogListener implements ShouldQueue
 {
-    private $apiUser;
-    private $apiPassword;
-    private $client;
-    private $response;
-    private $request;
+    private $naverBlog;
 
     public function __construct()
     {
-        $endPoint = 'https://api.blog.naver.com/xmlrpc';
-        $this->apiUser = env('NAVER-BLOG-ID');
-        $this->apiPassword = env('NAVER-BLOG-PASSWORD');
-        $this->client = new Client($endPoint);
-        $this->client->return_type = 'json';
-        $this->client->setSSLVerifyPeer(false);
+        $isSecret = app()->environment() === 'production';
+        $this->naverBlog = new NaverBlogXml(env('NAVER-BLOG-ID'), env('NAVER-BLOG-PASSWORD'), $isSecret);
     }
 
     public function handle(NaverBlog $event)
@@ -37,81 +27,6 @@ class NaverBlogListener implements ShouldQueue
         }
     }
 
-    /**
-     * @param $method
-     * @param $data
-     *
-     * @return $result
-     */
-    private function result($method, $data)
-    {
-        $this->request = new Request($method, $data);
-        $this->response = $this->client->send($this->request);
-
-        $result = new \stdClass();
-
-        $result->xml = $this->response->value();
-        $result->errno = $this->response->faultCode();
-        $result->errstr = $this->response->faultString();
-
-        return $result;
-    }
-
-    /**
-     * @param $context
-     *
-     * @return string image url Change context
-     */
-    private function getImages($context)
-    {
-        preg_match_all("/<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>/i", $context, $item);
-        $images = [];
-        if ($item[1] && count($item[1])) {
-            foreach ($item[1] as $image) {
-                $images[] = $image;
-            }
-        }
-
-        foreach ($images as $image) {
-            $tempUrl = $this->uploadMedia($image);
-            $replaceUrl = $tempUrl ?? $image;
-            $context = str_replace($image, $replaceUrl, $context);
-        }
-
-        return $context;
-    }
-
-    /**
-     * @param $post
-     * @param $context
-     *
-     * @return array struct
-     */
-    private function getStruct($post, $context)
-    {
-        $postUrl = route('post.show', optimus($post->id));
-        $context = nl2br($context);
-        $context = str_replace('<code', '<pre', $context);
-        $context = str_replace('</code>', '</pre>', $context);
-        $context = "<h2>{$post->subTitle}</h2>{$context}<br/><br/>자세한 이야기 보러가기<br/><a target='_blank' href=\"{$postUrl}\">{$postUrl}</a>";
-
-        $struct = [
-            'title' => new Value($post->title, 'string'),
-            'description' => new Value($context, 'string'),
-        ];
-
-        if ($category = optional($post->series)->title) {
-            $struct['categories'] = new Value(strip_tags(trim($category)), 'string');
-        }
-
-        if (count($post->tags)) {
-            $tags = implode(',', optional($post->tags)->pluck('name')->toArray());
-            $struct['tags'] = new Value(strip_tags(trim($tags)), 'string');
-        }
-
-        return $struct;
-    }
-
     /*
      * Naver Blog New Post
      */
@@ -122,25 +37,30 @@ class NaverBlogListener implements ShouldQueue
             return $this->edit($post);
         }
 
-        $method = 'metaWeblog.newPost';
+        $context = $this->changeContext($post);
+        $category = optional($post->series)->title;
+        $tags = count($post->tags) ? implode(',', optional($post->tags)->pluck('name')->toArray()) : [];
+        $postId = $this->naverBlog->newBlog($post->title, $context, $category, $tags);
 
-        $context = $this->getImages($post->context);
-        $struct = $this->getStruct($post, $context);
-
-        $data = [
-            new Value($this->apiUser, 'string'),
-            new Value($this->apiUser, 'string'),
-            new Value($this->apiPassword, 'string'),
-            new Value($struct, 'struct'),
-            new Value(true, 'boolean'),
-        ];
-
-        $result = $this->result($method, $data);
-
-        $post->naver = $result->xml->me['string'] ?? null;
+        $post->naver = $postId ?? null;
         $post->save();
 
         ModelChange::dispatch('post');
+    }
+
+    private function changeContext($post)
+    {
+        $postUrl = route('post.show', optimus($post->id));
+        $context = nl2br($post->context);
+        $context = str_replace('<code', '<pre', $context);
+        $context = str_replace('</code>', '</pre>', $context);
+
+        $views = [];
+        $views[] = "<h2>{$post->subTitle}</h2>{$context}";
+        $views[] = "자세한 이야기 보러가기";
+        $views[] = "<a target='_blank' href=\"{$postUrl}\">{$postUrl}</a>";
+
+        return implode('<br/>', $views);
     }
 
     /*
@@ -161,57 +81,13 @@ class NaverBlogListener implements ShouldQueue
         $this->new($post);
     }
 
-    /*
-     * Naver Blog Post Delete
-     */
     private function del($post)
     {
-        $method = 'blogger.deletePost';
-
         // 등록된 네이버 블로그가 없는 경우 취소
         if (!$post->naver) {
             return false;
         }
 
-        $data = [
-            new Value('', 'string'),
-            new Value($post->naver, 'string'),
-            new Value($this->apiUser, 'string'),
-            new Value($this->apiPassword, 'string'),
-            new Value(true, 'boolean'),
-        ];
-
-        $this->result($method, $data);
-    }
-
-    /*
-     * Naver File Server Upload
-     * return Naver Image Server Upload Image URL;
-     */
-    private function uploadMedia($url)
-    {
-        $tempFileName = explode('/', $url);
-        $name = $fileName ?? $tempFileName[count($tempFileName) - 1];
-        $mime = \Image::make($url)->mime();
-        $bits = file_get_contents($url);
-
-        $method = 'metaWeblog.newMediaObject';
-
-        $struct = array(
-            'bits' => new Value($bits, 'base64'),
-            'type' => new Value($mime, 'string'),
-            'name' => new Value($name, 'string'),
-        );
-
-        $media = array(
-            new Value($this->apiUser, 'string'),
-            new Value($this->apiUser, 'string'),
-            new Value($this->apiPassword, 'string'),
-            new Value($struct, 'struct'),
-        );
-
-        $result = $this->result($method, $media);
-
-        return $result->xml->me['struct']['url']->me['string'] ?? null;
+        $this->naverBlog->delBlog($post->naver);
     }
 }
