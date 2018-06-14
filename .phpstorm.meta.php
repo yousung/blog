8c2b05f7307ba5f9b41cf1f2baf4f64eebf7b1a3
yousung <nug22kr@gmail.com>
2018-06-14 10:19:09 +0900

    # composer 업데이트

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-06-14 10:13:24.
+    * Generated on 2018-06-14 10:17:50.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
